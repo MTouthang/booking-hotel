c7db0e79c69bba04a55459a1fa5ec0b53646a01c
Mamang touthang <huebart6@gmail.com>
2022-06-06 18:22:44 +0530

    frontEnd layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Booking from "./component/pages/Booking";
+
+import Footer from "./component/layout/Footer";
+import Home from "./component/pages/Home";
+import NavBar from "./component/layout/NavBar";
+import PageNotFound from "./component/pages/PageNotFound";
+import Signin from "./component/pages/Signin";
+import Signup from "./component/pages/Signup";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/bookings" element={<Booking />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ client/src/component/layout/NavBar.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavLink,
+  NavbarText,
+} from "reactstrap";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <Navbar color="info" expand="md" light className="p-4">
+        <NavbarBrand tag={Link} to="/" className="text-white mx-6">
+          HOTEL BOOKING APP
+        </NavbarBrand>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse navbar isOpen={isOpen}>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <NavLink tag={Link} to="/signup" className="text-white">
+                SIGNUP
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/signin" className="text-white">
+                SINGIN
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={Link} to="/bookings" className="text-white">
+                BOOKINGS
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>NameText</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
